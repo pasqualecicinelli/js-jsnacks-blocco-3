ce25fd6abc9bb701c634fe2207097889afaf9e0f
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-26 16:39:56 +0200

    feat third ex

--- JS/main.js
+++ JS/main.js
@@ -14,22 +14,42 @@ if (firstNumber > secondNumber) {
 
     console.log(random2);
 }*/
-
+/*
 const firstWorld = prompt('Inserisci una parola');
 const secondWorld = prompt('Inserisci la seconda parola');
 
 if (firstWorld.length == secondWorld.length) {
-    
+
     console.log(firstWorld + ' ' + secondWorld);
 
 } else if (firstWorld.length > secondWorld.length) {
-    
+
     console.log(firstWorld);
 
 } else if (secondWorld.length > firstWorld.length) {
-    
-    console.log(secondWorld);
-}
 
+    console.log(secondWorld);
+}*/
+
+let arrayVoid = [];
+let somma = 0;
+
+
+
+while (somma < 50) {
+
+    let numberUser = parseInt(prompt('Inserisci un numero'));
+
+    somma += numberUser;
+
+
+    console.log(somma)
+
+    if (somma < 50) {
+        arrayVoid.push(numberUser);
+    }
+
+}
+console.log(arrayVoid);
 
 
--- README.md
+++ README.md
@@ -19,3 +19,7 @@ la prima è maggiore della seconda stampo la prima parola
 
 # ALTRIMENTI SE 
 la seconda è maggiore della prima stampo la seconda parola
+
+# TRACCIA 3
+
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
